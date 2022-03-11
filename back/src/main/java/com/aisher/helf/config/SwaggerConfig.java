7feaeec23ac46fb5f741180fb669b326785b8e34
최영진 <cyjcmb98@naver.com>
2022-03-11 15:04:56 +0900

    update : swagger parameger name

--- back/src/main/java/com/aisher/helf/config/SwaggerConfig.java
+++ back/src/main/java/com/aisher/helf/config/SwaggerConfig.java
@@ -1,10 +1,19 @@
 package com.aisher.helf.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
@@ -23,12 +32,18 @@ import static com.google.common.collect.Lists.newArrayList;
  * API 문서 관련 swagger2 설정 정의.
  */
 @Configuration
+@RequiredArgsConstructor
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private final TypeResolver typeResolver;
+
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .alternateTypeRules(AlternateTypeRules
+                        .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
@@ -65,5 +80,18 @@ public class SwaggerConfig {
 //                .supportedSubmitMethods(newArrayList("get").toArray(new String[0])) // try it 기능 활성화 범위
 //                .operationsSorter(METHOD)
                 .build();
+    }
+    @Getter
+    @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
+
+        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
+        private List<String> sort;
     }
 }
