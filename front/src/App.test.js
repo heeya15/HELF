c68578a4e56e0ee44fbb3acd120a7d372a09206a
한성희 <bee1404@naver.com>
2022-03-21 13:34:21 +0900

    add : build up the frontend directory structure
    - 프론트앤드 디렉토리 구조 형성

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Main from './components/Main';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Main />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
