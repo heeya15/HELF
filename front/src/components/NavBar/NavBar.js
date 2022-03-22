82cbe8cb74b2e941aceb33029f96bb7388dd180f
손한기 <wwenxt@naver.com>
2022-03-22 15:54:45 +0900

    fix : Navbar logo 수정, 크기 수정, 글자 생상 및 밑줄 수정

--- front/src/components/NavBar/NavBar.js
+++ front/src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Link } from 'react-router-dom';
 import logo from '../../logo.png';
+import './NavBar.css';
 
 // 컴포넌트 import
 import SharedBoard from '../SharedBoard/SharedBoard'
@@ -13,12 +14,14 @@ function NavBar() {
   return (
     <div className='navBar'>
       <div>
-        <Link to="/"><img  src={ logo } alt="" /></Link>  
-        <Link to="/">게시판</Link>      
-        <Link to="/">MY식단</Link>        
+        <Link to="/"  className='anc'><img className="logo" src={ logo } alt="" /></Link>  
+        <Link to="/"  className='anc'>게시판</Link>      
+        <Link to="/"  className='anc'>MY식단</Link>        
         <Route path="/about" component={SharedBoard} />
+        </div>
+        <div>
+        <Link to="/"  className='anc'>PT 자세교정</Link>          
       </div>
-      <Link to="/#">PT 자세교정</Link>          
     </div>
     
   );
