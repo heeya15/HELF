ef9b7e0ed2df8581dd026713b677bce28bd2b413
손한기 <wwenxt@naver.com>
2022-03-22 16:18:41 +0900

    update : Router 추가git add . Main 에서 상단 Navbar 로 페이지 이동 가능, 이후 추가시 App.js 에 Router 요소 추가하는 형식으로 게시판 추가해 주세요

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,9 @@ import { Route, Link } from 'react-router-dom';
 
 // 컴포넌트 추가
 import NavBar from './components/NavBar/NavBar'
+import SharedBoard from './components/SharedBoard/SharedBoard'
+import Main from './components/Main/Main'
+
 // import Placeholder from 'react-bootstrap/Placeholder';
 
 
@@ -16,118 +19,12 @@ import NavBar from './components/NavBar/NavBar'
 function App() {
   return (
     <div className="App">
-      <div className='mainPage'>
+      
       {/* NavBar */}
       < NavBar/>
-
-      {/*  메인페이지 상단 이미지 */}
-      <div className='mainIMG'>
-        <img src={ logo } alt="" />  
-      </div>
+      <Route exact path="/" component={Main} />
+      <Route path="/SharedBoard" component={SharedBoard} />
       
-
-      {/*  메인페이지 상단 1 */}
-      <div className='welcome'>
-       <img  src={ logo } alt="" /> 
-       <div className='welcomeStart'>
-          <h1>우리사이트에 오신것을 환영합니다.</h1> 
-          <Button variant="success">빠른 시작</Button> 
-        </div> 
-       <img  src={ logo } alt="" /> 
-      </div>
-      
-      <hr/>
-      <div className='container'>
-        
-        {/* 메인페이지 상단 2 */}
-        <div className='welcome2'>
-          <h1>우리사이트에 오신것을 환영합니다.</h1> 
-          <div className="d-flex justify-content-around">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>     
-        </div>
-
-        <hr />
-    
-        {/* 메인페이지 상단 3 */}
-        <div className='welcome2'>
-          <h1>이런 효과를 기대합니다.</h1> 
-          <div className="d-flex justify-content-around">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>     
-        </div>
-        <hr />
-
-        </div>
-      </div>
     </div>
   );
 }
--- front/src/components/NavBar/NavBar.js
+++ front/src/components/NavBar/NavBar.js
@@ -15,9 +15,8 @@ function NavBar() {
     <div className='navBar'>
       <div>
         <Link to="/"  className='anc'><img className="logo" src={ logo } alt="" /></Link>  
-        <Link to="/"  className='anc'>게시판</Link>      
+        <Link to="/SharedBoard"  className='anc'>게시판</Link>      
         <Link to="/"  className='anc'>MY식단</Link>        
-        <Route path="/about" component={SharedBoard} />
         <Link to="/"  className='anc'>PT 자세교정</Link>          
       </div>
       <div>
