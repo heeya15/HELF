57c03717130fa49602bf7a776e145678d25279b7
손한기 <wwenxt@naver.com>
2022-03-23 16:55:02 +0900

    add : sharedboard counstructor 추가(board 내용 가져오기 구현)
    Former-commit-id: 86eba6d90d24f4c0c1800553566a8bde1f61d2aa

--- front/src/components/SharedBoard/SharedBoard.js
+++ front/src/components/SharedBoard/SharedBoard.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
 
-
-
-
-
-function SharedBoard() {
-  return (
-    <div>SharedBoard 입니다.</div>
+class SharedBoard extends Component {
+	constructor(props) {
+      super(props);
+      const token = sessionStorage.getItem("jwt");
+      axios.get("https://j6e102.p.ssafy.io:8080/api/shareboard/findAll", {
+            headers: {
+              Authorization: `Bearer ${ token }`
+            }
+        }).then((result)=>{ console.log(result.data) }).catch((err) => {console.log('앙댐;')})
+      console.log('Person Instance is created, and initialized');
+    }
+  
+  	render() {
+      return (
+        <div>SharedBoard 입니다.
+    </div>
   );
-}
-export default SharedBoard
+      ;
+    }
+} export default SharedBoard
