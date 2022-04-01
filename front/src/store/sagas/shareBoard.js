5d120de02d076262e6a2527cf7169c4aa03e5211
김광희 <hgi7201@naver.com>
2022-04-01 23:45:57 +0900

    add : Implementation of search for the 5 posts with the highest number of likes among shared posts on the shared bulletin board
    - 공유 게시판에 공유 게시글 중 좋아요 수가 가장 많은 5개의 게시글 조회 구현
    
    
    Former-commit-id: 5db7166f112db8413d42c081ddbf286cb6ca40de

--- front/src/store/sagas/shareBoard.js
+++ front/src/store/sagas/shareBoard.js
@@ -7,11 +7,15 @@ import {
 } from 'redux-saga/effects';    
 import {
     shareBoardRegisterAPI,
+    ShareBoardTopLikeAPI
 } from '../apis/shareBoard'
 import {
     SHARE_BOARD_REGISTER_REQUEST,
     SHARE_BOARD_REGISTER_SUCCESS,
     SHARE_BOARD_REGISTER_FAILURE,
+    SHARE_BOARD_LIKE_REQUEST,
+    SHARE_BOARD_LIKE_SUCCESS,
+    SHARE_BOARD_LIKE_FAILURE 
 } from '../modules/shareBoard';
 import swal from 'sweetalert'; // 예쁜 alert 창을 위해 사용
 
@@ -37,8 +41,29 @@ function* watchLoadShareBoardRegister() {
     yield takeLatest(SHARE_BOARD_REGISTER_REQUEST, loadShareBoardRegister);
 }
 
+function* loadShareBoardTopLikeList() {
+    try {
+        const result = yield call(ShareBoardTopLikeAPI);
+        console.log("요청들어옴?");
+        console.log(result);
+        yield put({
+          type: SHARE_BOARD_LIKE_SUCCESS ,
+          data: result,
+        });
+      } catch (error) {
+        yield put({
+          type: SHARE_BOARD_LIKE_FAILURE,
+        });
+      }
+}
+
+function* watchLoadShareBoardTopLike() {
+    yield takeLatest(SHARE_BOARD_LIKE_REQUEST , loadShareBoardTopLikeList);
+}
+
 export default function* ShareBoardSaga() {
     yield all([
         fork(watchLoadShareBoardRegister),
+        fork(watchLoadShareBoardTopLike)
     ]);
 }
