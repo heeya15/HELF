175c6b59d97910266961402bf7a126b6496177b9
한성희 <bee1404@naver.com>
2022-04-05 16:59:26 +0900

    fix : signup validation issue
    - 회원가입 유효성검사 체크 이슈 해결
    - 회원가입 완료 후, 회원가입 페이지 못 돌아가는 이슈 해결
    
    
    Former-commit-id: 0b3a0dfe97322ead1edd4beac275f3c3c7cd1386

--- front/src/store/modules/user.js
+++ front/src/store/modules/user.js
@@ -58,6 +58,7 @@ export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+export const SIGN_UP_RESET = 'SIGN_UP_RESET';
 
 export const EMAIL_CHECK_REQUEST = 'EMAIL_CHECK_REQUEST';
 export const EMAIL_CHECK_SUCCESS = 'EMAIL_CHECK_SUCCESS';
@@ -184,6 +185,11 @@ const reducer = (state = initialState, action) =>
       case SIGN_UP_FAILURE:
         draft.signUpLoading = false;
         draft.signUpError = action.error;
+        break;
+      case SIGN_UP_RESET:
+        draft.signUpLoading = false;
+        draft.signUpDone = false;
+        draft.signUpError = null;
         break;
       case EMAIL_CHECK_REQUEST:
         draft.emailCheckLoading = true;
